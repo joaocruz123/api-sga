009462a04e2bd0145988a472a43ca6c6fde8f377
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2021-05-05 15:04:16 -0300

    Criando os validations para os membros

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'AuthController@login');
 Route::post('/register', 'AuthController@register');
 
+Route::resource('membros', 'MembrosController');
+
 Route::group([ 'middleware' => 'auth:api'], function() {
     Route::get('logout', 'AuthController@logout');
     Route::get('user', 'AuthController@user');
-
-    Route::resource('membros', 'MembrosController');
 });
