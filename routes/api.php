5aab6be44bc914aafb7eca62172451f68a54399c
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2021-05-11 05:42:21 -0300

    Implantação do CORS

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', 'AuthController@login');
 Route::post('/register', 'AuthController@register');
 
-Route::resource('membros', 'MembrosController');
-Route::resource('cargos', 'CargosController');
 
 Route::group([ 'middleware' => 'auth:api'], function() {
     Route::get('logout', 'AuthController@logout');
     Route::get('user', 'AuthController@user');
+    Route::resource('membros', 'MembrosController');
+    Route::resource('cargos', 'CargosController');
 });
