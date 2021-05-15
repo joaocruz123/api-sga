cc3b92e1123c5150c79a82047d5959494764ba40
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2021-05-15 20:02:10 -0300

    Ajustes para as rotas de cargo

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::post('/register', 'AuthController@register');
 
 
 Route::group([ 'middleware' => 'auth:api'], function() {
-    Route::get('logout', 'AuthController@logout');
+    Route::post('logout', 'AuthController@logout');
     Route::get('user', 'AuthController@user');
     Route::resource('membros', 'MembrosController');
     Route::resource('cargos', 'CargosController');
